461f103800f68af8c78e0fb6ff0486934397eedb
Will <forbes8963@gmail.com>
2019-05-24 14:29:24 -0400

    add event listener to update all tabs when localStorage is modified

--- notes-edit.js
+++ notes-edit.js
@@ -2,8 +2,6 @@ const titleElement = document.querySelector('#note-title')
 const bodyElement = document.querySelector('#note-body')
 const removeElement = document.querySelector('#remove-note')
 const saveElement = document.querySelector('#save-note')
-
-
 const noteId = location.hash.substring(1)
 let notes = getSavedNotes()
 let note = notes.find(function (note){
@@ -31,4 +29,21 @@ removeElement.addEventListener('click', function(){
     removeNote(note.id)
     saveNotes(notes)
     location.assign('index.html')
+})
+
+window.addEventListener('storage', function (e) {
+    if (e.key === 'notes') {
+        notes = JSON.parse(e.newValue)
+        note = notes.find(function (note) {
+            return note.id === noteId
+        })
+
+        if (note === undefined) {
+            location.assign('index.html')
+        }
+
+        //populate text boxes
+        titleElement.value = note.title
+        bodyElement.value = note.body
+    }
 })
