42b25fbe06efa199923d850411b8bc72c71ff1bc
Will <forbes8963@gmail.com>
2019-05-24 14:25:13 -0400

    refector multiple queryselectors into constants

--- notes-edit.js
+++ notes-edit.js
@@ -1,6 +1,12 @@
+const titleElement = document.querySelector('#note-title')
+const bodyElement = document.querySelector('#note-body')
+const removeElement = document.querySelector('#remove-note')
+const saveElement = document.querySelector('#save-note')
+
+
 const noteId = location.hash.substring(1)
-const notes = getSavedNotes()
-const note = notes.find(function (note){
+let notes = getSavedNotes()
+let note = notes.find(function (note){
     return note.id === noteId
 })
 
@@ -8,22 +14,21 @@ if (note === undefined){
     location.assign('index.html')
 }
 
-document.querySelector('#note-title').value = note.title
-document.querySelector('#note-body').value = note.body
+//populate text boxes
+titleElement.value = note.title
+bodyElement.value = note.body
 
-document.querySelector('#save-note').addEventListener('click', function (e) {
-    note.body = document.querySelector('#note-body').value
-    note.title = document.querySelector('#note-title').value
+//save new note
+saveElement.addEventListener('click', function (e) {
+    note.body = bodyElement.value
+    note.title = titleElement.value
     saveNotes(notes)
     location.assign('index.html')
 })
 
-document.querySelector('#remove-note').addEventListener('click', function(){
+//remove note
+removeElement.addEventListener('click', function(){
     removeNote(note.id)
     saveNotes(notes)
     location.assign('index.html')
-})
-
-window.addEventListener('storage', function(e){
-    console.log('fuck')
 })
