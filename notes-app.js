a221bfdcd838a36ed052212605bbdcb6ed6a8d4a
Will <forbes8963@gmail.com>
2019-05-23 20:40:09 -0400

    first commit. add noteId as hash to URL

---
+++ notes-app.js
@@ -0,0 +1,27 @@
+const notes = getSavedNotes()
+
+const filters = {
+    searchText: ''
+}
+
+renderNotes(notes, filters)
+
+document.querySelector("#create-note").addEventListener("click", function(){
+    const noteId = uuidv4()
+    notes.push({
+        id: noteId,
+        title: '',
+        body: '',
+    })
+    saveNotes(notes)
+    location.assign(`edit.html#${noteId}`)
+})
+
+document.querySelector("#search-text").addEventListener('input', function(e){
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
+
+document.querySelector("#filter-by").addEventListener('change', function(e){
+    console.log(e.target.value)
+})
